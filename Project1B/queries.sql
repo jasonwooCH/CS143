e340853d313457a14742145bedeab0fdf707457f
jasonwooCH <smwlks@hotmail.com>
2015-10-21 01:19:34 -0700

    explicitly enforce foreign keys, reorder load to satisfy foreign key constraint

--- Project1B/queries.sql
+++ Project1B/queries.sql
@@ -1,21 +1,20 @@
--- first attempt
--- SELECT first, last
--- FROM Actor
--- WHERE id IN
--- (	SELECT aid id
--- 		FROM Movie M, MovieActor MA
---		WHERE title='Die Another Day' AND
---		  	  M.id = MA.mid	);
-
--- First Q answer
+-- First Query answer
 SELECT CONCAT_WS(' ', first, last)
 FROM Actor A, MovieActor MA, Movie M
 WHERE MA.mid = M.id AND title = 'Die Another Day' AND MA.aid = A.id;
 
 
--- Second Q
+-- Second Query
 SELECT COUNT(*)
-FROM ( SELECT aid
-	   FROM MovieActor
-	   GROUP BY aid
-	   HAVING COUNT(*) >= 2) S;
+FROM (SELECT aid
+	  FROM MovieActor
+	  GROUP BY aid
+	  HAVING COUNT(*) >= 2) S;
+
+-- Our Query
+-- Give me the first and last names of directors who also acted in the movies they directed
+SELECT first, last
+FROM Director
+WHERE id IN (SELECT MD.did
+	   		 FROM MovieDirector MD, MovieActor MA
+	   		 WHERE MD.did = MA.aid AND MD.mid = MA.mid);
--- Project1B/violate.sql
+++ Project1B/violate.sql
@@ -35,26 +35,32 @@ INSERT INTO Director
 VALUES (2, 'test', 'name','2018-06-16 00:00:00', NULL);
 
 -- MovieGenre foreign key constraint
--- by default, update is not allowed if foreign key constraint is violated
+-- Movie with id 7 does not exist in Movie table
 UPDATE MovieGenre
 SET mid = 7;
 
 -- MovieDirector foreign key constraint
--- by default, update is not allowed if foreign key constraint is violated
+-- Movie with id 7 does not exist in Movie table
 UPDATE MovieDirector
-SET mid = 7, did = 69;
+SET mid = 7;
+-- Director with id 7 does not exist in Director table
+UPDATE MovieDirector
+SET did = 7;
 
 -- MovieActor foreign key constraint
--- by default, update is not allowed if foreign key constraint is violated
+-- Movie with id 7 does not exist in Movie table
 UPDATE MovieActor
-SET mid = 93, aid = 69;
+SET mid = 7;
+-- Actor with id 2 does not exist in Actor table
+UPDATE MovieActor
+SET aid = 2;
 
 -- Review table foreign key constraint
--- by default, update is not allowed if foreign key constraint is violated
+-- Movie with id 7 does not exist in Movie table
 UPDATE Review
-SET mid = 93;
+SET mid = 7;
 
 -- Review check constraints
 -- Rating cannot be outside the range 0-5
 INSERT INTO Review 
-VALUES ('test', '1993-06-16 00:00:00', 2, -4, NULL);
+VALUES ('test', '2015-06-16 00:00:00', 2, -4, NULL);
